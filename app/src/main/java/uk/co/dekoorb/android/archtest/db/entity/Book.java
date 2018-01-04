ca7cbe914b6f210c9613bef3561e1c50819f79a2
Ed Brook <edbrook@gmail.com>
2018-01-04 17:13:30 +0000

    add required getters / setters

--- app/src/main/java/uk/co/dekoorb/android/archtest/db/entity/Book.java
+++ app/src/main/java/uk/co/dekoorb/android/archtest/db/entity/Book.java
@@ -15,4 +15,44 @@ public class Book {
     private String author;
     private String description;
     private boolean hasRead;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRead() {
+        return hasRead;
+    }
+
+    public void setRead(boolean hasRead) {
+        this.hasRead = hasRead;
+    }
 }
